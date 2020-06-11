30c1f3154b4498e7bdabdff0fe1600e7dcc1eff7
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-06-11 11:20:19 +0200

    Create category.php

---
+++ library/model/category.php
@@ -0,0 +1,22 @@
+<?php
+
+/* category table */
+
+class Category {
+
+    /* table fields */
+    public $id;
+    public $name;
+    public $creationDate;
+    public $updateDate;
+
+    /* set default value with constructor */
+    function __construct()
+    {
+        $this->id = 0;
+        $this->name = "";
+        $this->creationDate = date('Y-m-d H:i:s');
+        $this->updateDate = date('Y-m-d H:i:s');
+    }
+    
+}

630ff12c9b463370e698a85cae4dc4bc0db140fb
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-06-11 22:40:09 +0200

    Update userController.php

--- library/controller/userController.php
+++ library/controller/userController.php
@@ -9,7 +9,7 @@ class UserController {
         return $controller;
     }
 
-    /* select all userss from the database */
+    /* select all users from the database */
     public function fetchUsers() {
         $controller = $this->getController();
         $sql = "SELECT id, fullname, email, phone FROM user;";
@@ -25,24 +25,35 @@ class UserController {
         return $result;
     }
 
+    /* insert user */
     public function insertUser($values) {
         $controller = $this->getController();
-        $sql = "INSERT INTO user (fullname, email, phone, register_date) VALUES (?, ?, ?, ?);";
+        $sql = "INSERT INTO user (fullname, username, password, email, phone, register_date) VALUES (?, ?, ?, ?);";
         $type = 'ssss';
         $controller->insertRecord($sql, $values, $type);
     }
 
+    /* update user */
     public function updateUser($values) {
         $controller = $this->getController();
-        $sql = "UPDATE user SET fullname = ?, email = ?, phone = ?, update_date = ? WHERE id = ?;";
-        $type = 'ssss';
+        $sql = "UPDATE user SET fullname = ?, username = ?, password = ? email = ?, phone = ?, update_date = ? WHERE id = ?;";
+        $type = 'ssssss';
         $controller->insertRecord($sql, $values, $type);
     }
 
+    /* delete user */
     public function deleteUser($id) {
         $controller = $this->getController();
         $sql = "DELETE FROM user WHERE id = ?;";
         $controller->deleteRecord($sql, $id);
+    }
+
+    /* check username */
+    public function checkUsername($username) {
+        $controller = $this->getController();
+        $sql = "SELECT username FROM user WHERE username = ?;";
+        $result = $controller->selectRecord($sql, $username);
+        return $result->num_rows;
     }
 
 }

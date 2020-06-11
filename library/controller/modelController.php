1be2f62238839f10583686e013661fd140768ada
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-06-11 16:02:07 +0200

    Create modelController.php
    Class with methods that handle database data.

---
+++ library/controller/modelController.php
@@ -0,0 +1,91 @@
+<?php
+
+require_once __DIR__.'/../config.php';
+
+/* class with methods that handle database data */
+
+Class ModelController {
+
+    public function connect()
+    {
+        $db = Database::getInstance();
+        return $db;
+    }
+
+    public function fetchRecords($sql) {
+        $db = $this->connect();
+        $stmt = $db->stmt_init();
+        if(!$stmt->prepare($sql)) {
+            throw new \Exception( 'Prepare failed' );
+        } else {
+            $stmt->execute();
+        }
+
+        $result = $stmt->get_result();
+        if($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            return $result;
+        } else {
+            return null;
+        }
+    }
+
+    public function selectRecord($sql, $id) {
+        $db = $this->connect();
+        $stmt = $db->stmt_init();
+        if(!$stmt->prepare($sql)) {
+            throw new \Exception( 'Prepare failed' );
+        } else {
+            $stmt->bind_param("s", $id);
+            $stmt->execute();
+        }
+
+        $result = $stmt->get_result();
+        if($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            return $result;
+        } else {
+            return null;
+        }
+    }
+
+    // values is an array, so call_user_func_array is used to bind parameters
+    public function insertRecord($sql, $values, $type) {
+        $db = $this->connect();
+        $stmt = $db->stmt_init();
+        if(!$stmt->prepare($sql)) {
+            throw new \Exception( 'Prepare failed' );
+        } else {
+            call_user_func_array(array($stmt, "bind_param"), array_merge(array($type), $values));
+            $stmt->execute();
+        }
+    }
+
+    public function updateRecord($sql, $values, $type) {
+        $db = $this->connect();
+        $stmt = $db->stmt_init();
+        if(!$stmt->prepare($sql)) {
+            throw new \Exception( 'Prepare failed' );
+        } else {
+            call_user_func_array(array($stmt, "bind_param"), array_merge(array($type), $values));
+            $stmt->execute();
+        }
+    }
+
+    public function deleteRecord($sql, $id) {
+        $db = $this->connect();
+        $stmt = $db->stmt_init();
+        if(!$stmt->prepare($sql)) {
+            throw new \Exception( 'Prepare failed' );
+        } else {
+            $stmt->bind_param("s", $id);
+            $stmt->execute();
+        }
+
+        $result = $stmt->get_result();
+        if($row = $result->fetch_array(MYSQLI_ASSOC)) {
+            return $result;
+        } else {
+            return null;
+        }
+    }
+
+}

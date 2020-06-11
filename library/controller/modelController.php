d74043f1d2de8788399dcc7c73e63b951f0a7a43
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-06-11 23:05:16 +0200

    Update modelController.php

--- library/controller/modelController.php
+++ library/controller/modelController.php
@@ -29,7 +29,7 @@ Class ModelController {
         }
     }
 
-    public function selectRecord($sql, $id) {
+    public function oneParamRecord($sql, $id) {
         $db = $this->connect();
         $stmt = $db->stmt_init();
         if(!$stmt->prepare($sql)) {
@@ -48,7 +48,7 @@ Class ModelController {
     }
 
     // values is an array, so call_user_func_array is used to bind parameters
-    public function insertRecord($sql, $values, $type) {
+    public function arrayParamRecord($sql, $values, $type) {
         $db = $this->connect();
         $stmt = $db->stmt_init();
         if(!$stmt->prepare($sql)) {
@@ -56,35 +56,6 @@ Class ModelController {
         } else {
             call_user_func_array(array($stmt, "bind_param"), array_merge(array($type), $values));
             $stmt->execute();
-        }
-    }
-
-    public function updateRecord($sql, $values, $type) {
-        $db = $this->connect();
-        $stmt = $db->stmt_init();
-        if(!$stmt->prepare($sql)) {
-            throw new \Exception( 'Prepare failed' );
-        } else {
-            call_user_func_array(array($stmt, "bind_param"), array_merge(array($type), $values));
-            $stmt->execute();
-        }
-    }
-
-    public function deleteRecord($sql, $id) {
-        $db = $this->connect();
-        $stmt = $db->stmt_init();
-        if(!$stmt->prepare($sql)) {
-            throw new \Exception( 'Prepare failed' );
-        } else {
-            $stmt->bind_param("s", $id);
-            $stmt->execute();
-        }
-
-        $result = $stmt->get_result();
-        if($row = $result->fetch_array(MYSQLI_ASSOC)) {
-            return $result;
-        } else {
-            return null;
         }
     }
 

06cf7aebdfb1f80114f706a9a79624ed16c19bc0
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-06-11 21:11:32 +0200

    Create header.php

---
+++ library/view/includes/header.php
@@ -0,0 +1,49 @@
+<!-- NAVIGATION BAR -->
+<nav class="navbar navbar-expand navbar-dark bg-dark">
+
+    <ul class="navbar-nav  pl-0 ml-0 float-left">
+        <!-- dropdown Join -->
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Join
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <?php
+                    if(isset($_SESSION['userUsername']) || isset($_SESSION['adminUsername'])) {
+                        echo '<a class="dropdown-item" href="#">Log Out</a>';
+                    } else {
+                        echo '<a class="dropdown-item" href="#">User Log In</a>';
+                        echo '<a class="dropdown-item" href="#">Admin Log In</a>';
+                        echo '<div class="dropdown-divider"></div>';
+                        echo '<a class="dropdown-item" href="#">Sign Up</a>';
+                    }
+                ?>
+            </div>
+        </li>
+    </ul>
+
+    <!-- show the username of the person who is logged in, and manage button for admins -->
+    <ul class="navbar-nav pl-0 ml-0 float-left">
+        <?php
+            if(isset($_SESSION['userUsername'])) {
+                $userUsername = $_SESSION['userUsername'];
+                echo '<li class="nav-item active white-font">'.$userUsername.'</li>';
+            } else if(isset($_SESSION['adminUsername'])) {
+                $adminUsername = $_SESSION['adminUsername'];
+                echo '<li class="nav-item active btn"><a href="#" class="white-font">Manage</a></li>';
+                echo '<li class="nav-item active white-font my-auto mx-1">'.$adminUsername.'</li>';
+            } else {
+                '<li class="nav-item active"></li>';
+            }
+        ?>
+    </ul>
+
+</nav>
+
+<!-- JUMBOTRON -->
+<div class="jumbotron jumbotron-fluid jumbotron-header mb-0 py-2">
+    <div class="container">
+        <h2 class="display-4 text-center"><a href="../index.php" class="red-font">Online Library</a></h2>
+    </div>
+</div>

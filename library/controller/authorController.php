7abd3ac811b5ab00e184db7b976fdb0131217219
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-06-11 16:03:11 +0200

    Create authorController.php

---
+++ library/controller/authorController.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once __DIR__.'modelController.php';
+
+class AuthorController {
+
+    public function getController() {
+        $controller = new ModelController();
+        return $controller;
+    }
+
+    /* select all authors from the database */
+    public function fetchAuthors() {
+        $controller = $this->getController();
+        $sql = "SELECT id, name FROM author;";
+        $result = $controller->fetchRecords($sql);
+        return $result;
+    }
+
+    /* select author */
+    public function selectAuthor($id) {
+        $controller = $this->getController();
+        $sql = "SELECT id, name FROM author WHERE id = ?;";
+        $result = $controller->selectRecord($sql, $id);
+        return $result;
+    }
+
+    /* insert author */
+    public function insertAuthor($values) {
+        $controller = $this->getController();
+        $sql = "INSERT INTO author (name, creation_date) VALUES (?, ?);";
+        $type = 'ss';
+        $controller->insertRecord($sql, $values, $type);
+    }
+
+    /* update author */
+    public function updateAuthor($values) {
+        $controller = $this->getController();
+        $sql = "UPDATE author SET name = ?, update_date = ? WHERE id = ?;";
+        $type = 'ss';
+        $controller->insertRecord($sql, $values, $type);
+    }
+
+    /* delete author */
+    public function deleteRecord($id) {
+        $controller = $this->getController();
+        $sql = "DELETE FROM author WHERE id = ?;";
+        $controller->deleteRecord($sql, $id);
+    }
+    
+}

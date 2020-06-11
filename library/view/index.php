ff014d3c32554e8e6ad9f949b477b77922734544
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-06-11 22:38:50 +0200

    Update index.php

--- library/view/index.php
+++ library/view/index.php
@@ -10,9 +10,15 @@ session_status() === PHP_SESSION_ACTIVE ? TRUE : session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- BOOTSTRAP -->
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-    <!-- BOOTSTRAP JS -->
-    <script href="../assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
+    crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+    crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" 
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" 
+    crossorigin="anonymous"></script>
     <!-- STYLE -->
     <link href="../assets/css/style.css" rel="stylesheet" />
     <!-- JQUERY -->

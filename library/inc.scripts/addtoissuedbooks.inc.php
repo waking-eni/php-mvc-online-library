e398565873d6b922dc813b6ae2ed0bb99278c158
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-06-12 13:19:26 +0200

    Create addtoissuedbooks.inc.php

---
+++ library/inc.scripts/addtoissuedbooks.inc.php
@@ -0,0 +1,34 @@
+<?php
+session_status() === PHP_SESSION_ACTIVE ? TRUE : session_start();
+require_once __DIR__.'/../controller/issuedBooksController.php';
+
+/* script for book issuing */
+
+if(isset($_POST['addtoissuedbooks'])) {
+
+    try {
+        $issuedBooksCon = new IssuedBooksController();
+    } catch(Exception $e) {
+        echo 'Caught exception: '.$e->getMessage();
+    }
+
+    $userId = $_SESSION['userId'];
+    $bookId = $_SESSION['bookId'];
+    $fine = 5;
+    $issuedDate = date('Y-m-d H:i:s');
+    $returnDate = date("Y-m-d", strtotime('+1 month', $issuedDate));
+
+    if(empty($userId) || empty($bookId) || empty($issuedDate) || empty($returnDate)) {
+        header("Location: ../view/index.php?error=emptyfields");
+	    exit();
+    } else {
+        $values = array($issuedDate, $returnDate, $fine, $bookId, $userId);
+        $issuedBooksCon->insertIssuedBook($values);
+        header("Location: ../view/index.php?add=success");
+	    exit();
+    }
+
+} else {
+    header("Location: ../view/index.php");
+	exit();
+}

3786d22b20e249af9e46f8c82ecb17decbc8a8b3
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-06-12 21:56:41 +0200

    Update loginUser.php

--- library/view/loginUser.php
+++ library/view/loginUser.php
@@ -82,6 +82,15 @@ function validate() {
 }
 </script>
 
+<?php
+    //does the user exist
+    $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    if(strpos($actual_link, 'nouser')) {
+        echo '<script>document.getElementById("userMailName").innerHTML = "User doesn\'t exist";</script>';
+    }
+
+?>
+
 </body>
 
 </html>
